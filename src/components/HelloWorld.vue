2ebd787ae1a5a4d16ef5e42d2bbcbaeefcacdb10
wangjian <wangjian@eazytec.com>
2022-04-02 14:16:01 +0800

    【search result page】

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,46 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div>
+      <h2> Filter Learn Vue Articles</h2>
+      <input 
+        type='text' 
+        placeholder='Filter Search' 
+        v-model='query'
+      />
+      <br>
+      <button @click='reset'> Reset </button>
+      <search-results :query='query'/>
+  </div>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
+
+<script >
+import { ref } from 'vue'
+import SearchResults from './SearchResults.vue'
+
+export default {
+  components:{
+    SearchResults
+  },
+  setup(){
+    const query = ref('')
+    const reset = (evt)=>{
+      query.value = ''
+    }
+
+    return{
+      reset,
+      query
+    }
+
+  }
+
 }
+
+</script>
+
+<style scoped>
+ input[type='text'] {
+    margin-bottom: 20px;
+    padding: 10px;
+  }
 </style>
