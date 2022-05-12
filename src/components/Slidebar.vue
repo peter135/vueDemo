b1d58e7d4a9f8e64cd8a8ac1eabb4bedad1dcf61
wangjian <wangjian@eazytec.com>
2022-05-12 10:24:09 +0800

    【update css】

--- src/components/Slidebar.vue
+++ src/components/Slidebar.vue
@@ -1,10 +1,26 @@
 <template>
-    <aside>
-        slidebar
+    <aside :class="`${is_expanded &&'is-expanded'}`">
+       <div class="logo">
+           <img src="../assets/logo.png" alt="vue">
+       </div>
+       <div class="menu-toggle-wrap">
+           <button class="menu-toggle" @click="ToggleMenu">
+               <span class="material-icons">
+                   keyboard_double_arrow_right
+               </span>
+           </button>
+       </div>
     </aside>
 </template>
 
 <script setup>
+import{ref} from 'vue'
+
+const is_expanded = ref(false)
+
+const ToggleMenu = ()=> {
+    is_expanded.value = !is_expanded.value
+}
 
 </script>
 
@@ -21,6 +37,44 @@ aside {
     color: var(--light);
 
     transition: 0.2s ease-out;
+
+    .logo {
+        margin-bottom: 1rem;
+        img {
+            width: 2rem;
+        }   
+
+    }
+
+    .menu-toggle-wrap{
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 1rem;
+
+        position:relative;
+        top: 0;
+        transition: 0.2s ease-out;
+
+        .menu-toggle {
+            transition: 0.2s ease-out;
+            .material-icons{
+                font-size: 2rem;
+                color: var(--light);
+            }
+        }
+    }
+
+    &.is-expanded {
+        width: var(--slidebar-width);
+        
+        .menu-toggle-wrap{
+            top: -3rem;
+            .menu-toggle{
+                transform: rotate(-180deg);
+            }
+        }
+    }
+
 
     @media (max-width:768px) {
         position: fixed;
