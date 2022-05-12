7e2d6236e23c00c256179599a3fccc8c1d211d11
wangjian <wangjian@eazytec.com>
2022-05-12 08:49:51 +0800

    【update step】

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,35 @@ import Slidebar from './components/slidebar.vue'
 
 
 <style lang="scss" scoped>
+
+:root{
+  --primary:#4ade80;
+  --primary-alt:#22c55e;
+  --grey:#64748b;
+  --dark:#4ade80;
+  --dark-alt:#334155;
+  --light:#f1f5f9;
+  --slidebar-width:300px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: var(--light);
+}
+
+button {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+
 .app {
   display: flex;
   main {
--- src/components/Slidebar.vue
+++ src/components/Slidebar.vue
@@ -9,5 +9,22 @@
 </script>
 
 <style lang="scss" scoped>
+aside {
+    display:flex;
+    flex-direction: column;
+    width: calc(2rem + 32px);
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 1rem;
 
+    background-color: var(--dark);
+    color: var(--light);
+
+    transition: 0.2s ease-out;
+
+    @media (max-width:768px) {
+        position: fixed;
+        z-index: 99;
+    }
+}
 </style>
