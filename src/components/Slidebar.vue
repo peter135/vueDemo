677e70cfea76b3006f47b07cddde071cb43fefa1
wangjian <wangjian@eazytec.com>
2022-05-12 14:08:41 +0800

    【finish】

--- src/components/Slidebar.vue
+++ src/components/Slidebar.vue
@@ -10,16 +10,45 @@
                </span>
            </button>
        </div>
+       <h3>Menu</h3>
+       <div class="menu">
+           <router-link class="button" to="/">
+               <span class="material-icons">home</span>
+               <span class="text">home</span>
+           </router-link>
+           <router-link class="button" to="/about">
+               <span class="material-icons">visibility</span>
+               <span class="text">About</span>
+           </router-link>
+           <router-link class="button" to="/team">
+               <span class="material-icons">group</span>
+               <span class="text">Team</span>
+           </router-link>
+            <router-link class="button" to="/contact">
+               <span class="material-icons">email</span>
+               <span class="text">Contact</span>
+           </router-link>
+       </div>
+
+       <div class="flex"></div>
+       <div class="menu">
+          <router-link class="button" to="/settings">
+              <span class="material-icons">settings</span>
+              <span class="text">Settings</span>
+          </router-link>
+       </div>
+
     </aside>
 </template>
 
 <script setup>
 import{ref} from 'vue'
 
-const is_expanded = ref(false)
+const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
 
 const ToggleMenu = ()=> {
     is_expanded.value = !is_expanded.value
+    localStorage.setItem("is_expanded",is_expanded.value)
 }
 
 </script>
@@ -60,6 +89,66 @@ aside {
             .material-icons{
                 font-size: 2rem;
                 color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            &:hover {
+                .material-icons{
+                     color: var(--primary);
+                     transform:translateX(0.5rem)
+                }
+            }
+        }
+    }
+
+    h3,.button .text {
+        opacity: 0;
+        transition: 0.3s ease-out;
+    }
+
+    h3 {
+        color: var(--grey);
+        font-size: 0.875rem;
+        margin-bottom: 0.5rem;
+        text-transform: uppercase;
+    }
+
+    .flex {
+        flex: 1 1 0;
+    }
+
+    .menu {
+        margin: 0 -1rem;
+
+        .button {
+            display: flex;
+            align-items: center;
+            text-decoration: none;
+
+            padding: 0.5rem 1rem;
+            transition: 0.2s ease-out;
+
+            .material-icons {
+                font-size: 2rem;
+                color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            .text {
+                color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            &:hover, &.router-link-exact-active {
+                background-color: var(--dark-alt);
+                .material-icons, .text{
+                     color: var(--primary);
+                }
+
+            }
+
+            &.router-link-exact-active {
+                border-right: 5px solid var(--primary);
             }
         }
     }
@@ -71,6 +160,16 @@ aside {
             top: -3rem;
             .menu-toggle{
                 transform: rotate(-180deg);
+            }
+        }
+
+        h3,.button .text {
+            opacity: 1;
+        }
+
+        .button {
+            .material-icons {
+                margin-right: 1rem;
             }
         }
     }
